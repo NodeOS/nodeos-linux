df5fd3f6b8c76f58a73d9471c9ca34a70126a04a
Josh <joshgarde@gmail.com>
2016-12-11 18:55:57 -0800

    Initial Commit
    Version 0.1.0

---
+++ install.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var async   = require('async')
+var fs      = require('fs-extra')
+var manager = require('download-manager')
+
+const LINUX_VERSION    = "4.8.5"
+const LINUX_URL    = "https://www.kernel.org/pub/linux/kernel/v4.x/linux-"+LINUX_VERSION+".tar.gz"
+const LINUX_SHA256 = 'b8dd5365c9f4319d41781f65f27b116ae7981d791bbad46491617fee0f87f8cc'
+
+var downloads =
+[
+  {
+    name: 'linux',
+    url: LINUX_URL,
+    sha256: LINUX_SHA256
+  }
+]
+
+manager(downloads, function(error)
+{
+  if(error)
+  {
+    console.trace(error)
+    process.exit(1)
+  }
+})
---
+++ readme.md
@@ -0,0 +1,9 @@
+# nodeos-linux
+
+Downloads Linux source for use with compiling parts of NodeOS
+
+## Why?
+
+Because multiple packages within NodeOS rely on kernel headers not found within
+all operating systems (eg. OS X). This package simply downloads the Linux source
+into a central location to be used by all packages.
